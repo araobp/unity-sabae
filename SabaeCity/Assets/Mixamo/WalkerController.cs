a8989a619ad1fb2a476a833fab0373bb930f7e67
arao <araobp@users.noreply.github.com>
2021-01-24 17:12:41 +0900

    Add arrow keys on the signalling server side

--- SabaeCity/Assets/Mixamo/WalkerController.cs
+++ SabaeCity/Assets/Mixamo/WalkerController.cs
@@ -43,34 +43,182 @@ public class WalkerController : MonoBehaviour
         }
     }
 
-    public void Walk()
+    void Walk()
     {
         animator.SetTrigger("Walk");
     }
 
-    public void Stop()
+    void Stop()
     {
         animator.SetTrigger("Stop");
     }
 
-    public void TurnLeft()
+    void TurnLeft()
     {
         turningLeft = true;
     }
     
-    public void TurnRight()
+    void TurnRight()
     {
         turningRight = true;
     }
 
-    public void TurnLeftStop()
+    void TurnLeftStop()
     {
         turningLeft = false;
     }
 
-    public void TurnRightStop()
+    void TurnRightStop()
     {
         turningRight = false;
+    }
+
+    /***** Buttons *****/
+
+    // 1
+    public void ButtonAClick1()
+    {
+
+    }
+
+    // 5
+    public void ButtonATouchStart5()
+    {
+
+    }
+
+    // 6
+    public void ButtonATouchEnd6()
+    {
+
+    }
+
+    // 2
+    public void ButtonBClick2()
+    {
+
+    }
+
+    // 7
+    public void ButtonBTouchStart7()
+    {
+
+    }
+
+    // 8
+    public void ButtonBTouchEnd8()
+    {
+
+    }
+
+    // 3
+    public void ButtonXClick3()
+    {
+
+    }
+
+    // 9
+    public void ButtonXTouchStart9()
+    {
+
+    }
+
+    // 10
+    public void ButtonXTouchEnd10()
+    {
+
+    }
+
+    // 4
+    public void ButtonYClick4()
+    {
+
+    }
+
+    // 11
+    public void ButtonYTouchStart11()
+    {
+
+    }
+
+    // 12
+    public void ButtonYTouchEnd12()
+    {
+
+    }
+
+    /***** Arrow Keys *****/
+
+    // 13
+    public void ArrowDownClick13()
+    {
+
+    }
+
+    // 17
+    public void ArrowDownTouchStart17()
+    {
+
+    }
+
+    // 18
+    public void ArrowDownTouchEnd18()
+    {
+
+    }
+
+    // 14
+    public void ArrowRightClick14()
+    {
+
+    }
+
+    // 19
+    public void ArrowRightTouchStart19()
+    {
+        TurnRight();
+    }
+
+    // 20
+    public void ArrowRightTouchEnd20()
+    {
+        TurnRightStop();
+    }
+
+    // 15
+    public void ArrowLeftClick15()
+    {
+
+    }
+
+    // 21
+    public void ArrowLeftTouchStart21()
+    {
+        TurnLeft();
+    }
+
+    // 22
+    public void ArrowLeftTouchEnd22()
+    {
+        TurnLeftStop();
+    }
+
+    // 16
+    public void ArrowUpClick16()
+    {
+
+    }
+
+    // 23
+    public void ArrowUpClickTouchStart23()
+    {
+        Walk();
+    }
+
+    // 24
+    public void ArrowUpClickTouchEnd24()
+    {
+        Stop();
     }
 
 }
