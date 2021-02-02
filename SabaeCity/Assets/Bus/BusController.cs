78f7d2aca9df0cb26790ceb5411e5754c69e5efc
arao <araobp@users.noreply.github.com>
2021-02-02 21:02:44 +0900

    update

---
+++ SabaeCity/Assets/Bus/BusController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    Rigidbody rb;
+
+    [System.Serializable]
+    public class AxleInfo
+    {
+        public WheelCollider leftWheel;
+        public WheelCollider rightWheel;
+        public bool motor; // is this wheel attached to motor?
+        public bool steering; // does this wheel apply steer angle?
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                if (motor >= 0)
+                {
+                    axleInfo.leftWheel.brakeTorque = 0;
+                    axleInfo.rightWheel.brakeTorque = 0;
+                    axleInfo.leftWheel.motorTorque = motor;
+                    axleInfo.rightWheel.motorTorque = motor;
+                } else
+                {
+                    axleInfo.leftWheel.brakeTorque = -motor;
+                    axleInfo.rightWheel.brakeTorque = -motor;
+                }
+
+            }
+        }
+    }
+}
