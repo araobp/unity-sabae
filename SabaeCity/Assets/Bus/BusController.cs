10afd0535c27a4a5807c9664785b25ff2fed66bd
arao <araobp@users.noreply.github.com>
2021-02-14 19:56:20 +0900

    Remote VR support, focus on console game

--- SabaeCity/Assets/Bus/BusController.cs
+++ SabaeCity/Assets/Bus/BusController.cs
@@ -36,25 +36,6 @@ public class BusController : MonoBehaviour
         animator = GetComponentInParent<Animator>();
     }
 
-    void EvtHandler(string evt)
-    {
-        Debug.Log(evt);
-
-        string[] cmdValue = evt.Split(':');
-        string cmd = cmdValue[0];
-        float value = 0F;
-        if (cmdValue.Length > 1)
-        {
-            value = float.Parse(cmdValue[1]);
-        }
-
-        switch (cmd)
-        {
-
-        }
-    }
-
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha6))
@@ -78,29 +59,8 @@ public class BusController : MonoBehaviour
 
     void FixedUpdate()
     {
-        float axisVertical = 0F;
-        float axisHorizontal = 0F;
-        float axisVertical1 = Input.GetAxis("Vertical");
-        float axisHorizontal1 = Input.GetAxis("Horizontal");
-        float axisVertical2 = Input.GetAxis("Trigger");
-        float axisHorizontal2 = Input.GetAxis("LeftAnalogX");
-
-        if (axisVertical1 != 0)
-        {
-            axisVertical = axisVertical1;
-        } else if (axisVertical2 != 0)
-        {
-            axisVertical = axisVertical2;
-        }
-
-        if (axisHorizontal1 != 0)
-        {
-            axisHorizontal = axisHorizontal1;
-        }
-        else if (axisHorizontal2 != 0)
-        {
-            axisHorizontal = axisHorizontal2;
-        }
+        float axisVertical = Input.GetAxis("Trigger");
+        float axisHorizontal = Input.GetAxis("LeftAnalogX");
 
         float motor = maxMotorTorque * axisVertical;
         float steering = maxSteeringAngle * axisHorizontal;
--- SabaeCity/Assets/DigitalTwinController.cs
+++ SabaeCity/Assets/DigitalTwinController.cs
@@ -1,11 +1,5 @@
 ﻿using UnityEngine;
 
-public enum Actor
-{
-    GUIDE,
-    BUS
-}
-
 public class DigitalTwinController : MonoBehaviour
 {
     public GameObject guide;
@@ -15,16 +9,6 @@ public class DigitalTwinController : MonoBehaviour
     public GameObject guideCamera;
 
     public Canvas canvas;
-
-    BusController m_busController;
-    GuideController m_guideController;
-
-    Transform m_originalParent;
-    Quaternion m_originalBusCameraRotation;
-    Vector3 m_originalRenderStreamingCameraPosition;
-    Quaternion m_originalRenderStreamingCameraRotation;
-
-    Actor m_actor;
 
     private void enableGuide(bool state)
     {
@@ -48,25 +32,15 @@ public class DigitalTwinController : MonoBehaviour
 
     private void Start()
     {
-        m_guideController = guide.GetComponent<GuideController>();
-        m_busController = bus.GetComponent<BusController>();
-
         if (Menu.sceneSelection == SceneSelection.Walk)
         {
-            m_actor = Actor.GUIDE;
             enableBus(false);
             enableGuide(true);
         } else
         {
-            m_actor = Actor.BUS;
             enableBus(true);
             enableGuide(false);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
     }
 
 }
